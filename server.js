e634c8d0f6a9095f424f1e7fa9e31262714fafe6
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-01 12:48:03 +0800

    base

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+require('dotenv').config()
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Home of the Atlas API');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Running on Port " + process.env.PORT)
 })
