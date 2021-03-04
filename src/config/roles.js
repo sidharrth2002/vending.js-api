e268b80ee1b9bb1ff9c09c8d8028ab5c98bb7a66
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-04 14:20:21 +0800

    added vending to routes, roles

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,8 +1,8 @@
 const roles = ['user', 'admin'];
 
 const roleRights = new Map();
-roleRights.set(roles[0], []);  // 'getVendingMachines', 'getVendingMachineByID'
-roleRights.set(roles[1], ['getUsers', 'manageUsers', 'getComplaints', 'updateComplaints', 'updateVendingMachineByID']);
+roleRights.set(roles[0], []);
+roleRights.set(roles[1], ['getUsers', 'manageUsers', 'getComplaints', 'updateComplaints', 'getVendingMachines', 'getVendingMachineByID', 'updateVendingMachineByID']);
 
 module.exports = {
   roles,
