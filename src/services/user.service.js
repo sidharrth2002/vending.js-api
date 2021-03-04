094456101dfb85e0eb898687479bbd55100e2bfa
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-04 14:39:59 +0800

    create technician base code

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -11,8 +11,19 @@ const createUser = async (userBody) => {
   if (await User.isEmailTaken(userBody.email)) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
   }
-  const user = await User.create(userBody);
-  return user;
+  //continue this based on model
+  let newTechnician = new User({
+    email: userBody.email,
+    address: {
+      latitude: userBody.latitude,
+      longitude: userBody.longitude
+    },
+    role: userBody.role,
+    workingHours: [parseInt(userBody.workingHoursStart), parseInt(userBody.workingHoursEnd)]
+  })
+  
+  await newTechnician.save();
+  return newTechnician;
 };
 
 /**
