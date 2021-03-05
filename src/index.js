c4378f603065752da191fcb4796d9a04c5d101e5
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-06 00:41:24 +0800

    dynamic port for heroku

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const logger = require('./config/logger');
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
+  server = app.listen(process.env.PORT || 3000, () => {
+    logger.info(`Listening to port ${process.env.PORT || 3000}`);
   });
 });
 
