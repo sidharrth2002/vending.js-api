4ae0ca89f1099a3badcedfe6caf6516b3b7c291f
Mak Yen Wei <shaonmak@gmail.com>
2021-03-06 11:52:03 +0800

    feat: giving roles for technician to update appointment

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,7 +1,7 @@
 const roles = ['user', 'admin'];
 
 const roleRights = new Map();
-roleRights.set(roles[0], []);
+roleRights.set(roles[0], ['updateAppointment']);
 roleRights.set(roles[1], ['getUsers', 'manageUsers', 'getComplaints', 'updateComplaints', 'getVendingMachines', 'getVendingMachineByID', 'updateVendingMachineByID', 'createVendingMachine']);
 
 module.exports = {
