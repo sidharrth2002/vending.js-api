285058f8d461e803c6ab5ac18e476053f106d429
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-07 03:16:13 +0800

    local

--- src/routes/v1/map.route.js
+++ src/routes/v1/map.route.js
@@ -10,33 +10,39 @@ router.post('/getroute/:id', async(req, res) => {
     let technician = req.params.id;
     let appointments = await appointmentService.getAppointmentByUserId(technician);
 
-    let querystring = 'https://maps.googleapis.com/maps/api/directions/json?';
-    querystring += 'origin=';
-    querystring = querystring + startingLat.toString() + ',' + startingLong.toString();
-    querystring += '&waypoints=';
+    if(appointments.length > 0) {
+        let querystring = 'https://maps.googleapis.com/maps/api/directions/json?';
+        querystring += 'origin=';
+        querystring = querystring + startingLat + ',' + startingLong;
+        querystring += '&waypoints=';
 
-    for(let i = 0; i < appointments.length; i++) {
-        let appointment = appointments[i];
-        if(i != appointments.length - 1) {
-            querystring = querystring + appointment.vendingMachine.location.latitude + ',' + appointment.vendingMachine.location.longitude;
+        for(let i = 0; i < appointments.length; i++) {
+            let appointment = appointments[i];
+            if(i != appointments.length - 1) {
+                querystring = querystring + appointment.vendingMachine.location.latitude + ',' + appointment.vendingMachine.location.longitude;
+            }
+            if(i != appointments.length - 2) {
+                querystring += '|';
+            }        
         }
-        if(i != appointments.length - 2) {
-            querystring += '|';
-        }        
-    }
-    querystring += '&destination=';
-    console.log(appointments);
-    querystring += appointments[appointments.length - 1].vendingMachine.location.latitude;
-    querystring += ','
-    querystring += appointments[appointments.length - 1].vendingMachine.location.longitude;
-    querystring += '&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0';
-    console.log(querystring);
+        querystring += '&destination=';
+        console.log(appointments);
+        querystring += appointments[appointments.length - 1].vendingMachine.location.latitude;
+        querystring += ','
+        querystring += appointments[appointments.length - 1].vendingMachine.location.longitude;
+        querystring += '&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0';
+        console.log(querystring);
+        axios.get('https://maps.googleapis.com/maps/api/directions/json?destination=5.3332,100.3066&origin=5.3384,100.4306&waypoints=5.3433057049009545,100.43362052585397&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0')
+        .then(response => {
+            console.log(response.data.routes[0].overview_polyline.points);
+            console.log(`https://maps.googleapis.com/maps/api/staticmap?size=600x400&zoom=12&path=enc:${response.data.routes[0].overview_polyline.points}&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0`);
+            res.send(`https://maps.googleapis.com/maps/api/staticmap?size=600x400&zoom=12&path=enc:${response.data.routes[0].overview_polyline.points}&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0`)
+        })
+    } else {
+        console.log('dfhsdj');
 
-    axios.get('https://maps.googleapis.com/maps/api/directions/json?destination=5.3332,100.3066&origin=5.3384,100.4306&waypoints=5.3433057049009545,100.43362052585397&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0')
-    .then(response => {
-        console.log(response.data.routes[0].overview_polyline.points);
-        res.send(`https://maps.googleapis.com/maps/api/staticmap?size=600x400&zoom=12&path=enc:${response.data.routes[0].overview_polyline.points}&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0`)
-    })
+        res.send('Nothing to show');
+    }
 })
 
 module.exports = router;
