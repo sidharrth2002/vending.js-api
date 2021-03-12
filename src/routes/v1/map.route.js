ac63fcfe534c87218434caf411cdc063f4aa9b8c
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-03-12 16:43:09 +0800

    fix route calculation

--- src/routes/v1/map.route.js
+++ src/routes/v1/map.route.js
@@ -32,7 +32,7 @@ router.post('/getroute/:id', async(req, res) => {
         querystring += appointments[appointments.length - 1].vendingMachine.location.longitude;
         querystring += '&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0';
         console.log(querystring);
-        axios.get('https://maps.googleapis.com/maps/api/directions/json?destination=5.3332,100.3066&origin=5.3384,100.4306&waypoints=5.3433057049009545,100.43362052585397&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0')
+        axios.get(querystring)
         .then(response => {
             console.log(response.data.routes[0].overview_polyline.points);
             console.log(`https://maps.googleapis.com/maps/api/staticmap?size=600x400&zoom=12&path=enc:${response.data.routes[0].overview_polyline.points}&key=AIzaSyBN5MoqYOqVjmxji7ZcV6aJhiTzcYfgpR0`);
